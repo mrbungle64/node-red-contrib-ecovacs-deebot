f1b13b9e896bcf0a2677a7ddf5f6eaa4731d77b3
mrbungle64 <mrb1232@posteo.de>
2020-11-01 17:57:56 +0100

    Bugfix

--- ecovacs-deebot.js
+++ ecovacs-deebot.js
@@ -6,7 +6,7 @@ module.exports = function (RED) {
     let connection = false;
 
     function connect(node) {
-        this.vacbot = null;
+        node.vacbot = null;
 
         node.status({
             fill: 'yellow',
@@ -23,100 +23,100 @@ module.exports = function (RED) {
         api.connect(node.account.mail, password_hash).then(() => {
             api.devices().then((devices) => {
                 let vacuum = devices[node.config.deviceNumber];
-                this.vacbot = api.getVacBot(api.uid, EcoVacsAPI.REALM, api.resource, api.user_access_token, vacuum, continent);
-                this.vacbot.on("ready", (event) => {
+                node.vacbot = api.getVacBot(api.uid, EcoVacsAPI.REALM, api.resource, api.user_access_token, vacuum, continent);
+                node.vacbot.on("ready", (event) => {
                     connection = true;
                     node.status({
                         fill: 'green',
                         shape: 'dot',
                         text: 'Connected',
                     });
-                    this.vacbot.run("BatteryState");
-                    this.vacbot.on("BatteryInfo", (value) => {
+                    node.vacbot.run("BatteryState");
+                    node.vacbot.on("BatteryInfo", (value) => {
                         let msg = {
                             type: "BatteryInfo",
                             payload: value
                         };
                         node.send(msg);
                     });
-                    this.vacbot.on("CleanReport", (value) => {
+                    node.vacbot.on("CleanReport", (value) => {
                         let msg = {
                             type: "CleanReport",
                             payload: value
                         };
                         node.send(msg);
                     });
-                    this.vacbot.on("ChargeState", (value) => {
+                    node.vacbot.on("ChargeState", (value) => {
                         let msg = {
                             type: "ChargeState",
                             payload: value
                         };
                         node.send(msg);
                     });
-                    this.vacbot.on("LifeSpan_filter", (value) => {
+                    node.vacbot.on("LifeSpan_filter", (value) => {
                         let msg = {
                             type: "LifeSpan_filter",
                             payload: Math.round(value)
                         };
                         node.send(msg);
                     });
-                    this.vacbot.on("LifeSpan_main_brush", (value) => {
+                    node.vacbot.on("LifeSpan_main_brush", (value) => {
                         let msg = {
                             type: "LifeSpan_main_brush",
                             payload: Math.round(value)
                         };
                         node.send(msg);
                     });
-                    this.vacbot.on("LifeSpan_side_brush", (value) => {
+                    node.vacbot.on("LifeSpan_side_brush", (value) => {
                         let msg = {
                             type: "LifeSpan_side_brush",
                             payload: Math.round(value)
                         };
                         node.send(msg);
                     });
-                    this.vacbot.on("WaterLevel", (value) => {
+                    node.vacbot.on("WaterLevel", (value) => {
                         let msg = {
                             type: "WaterLevel",
                             payload: value
                         };
                         node.send(msg);
                     });
-                    this.vacbot.on("WaterBoxInfo", (status) => {
+                    node.vacbot.on("WaterBoxInfo", (status) => {
                         let msg = {
                             type: "WaterBoxInfo",
                             payload: status
                         };
                         node.send(msg);
                     });
-                    this.vacbot.on("DustCaseInfo", (status) => {
+                    node.vacbot.on("DustCaseInfo", (status) => {
                         let msg = {
                             type: "DustCaseInfo",
                             payload: status
                         };
                         node.send(msg);
                     });
-                    this.vacbot.on("SleepStatus", (status) => {
+                    node.vacbot.on("SleepStatus", (status) => {
                         let msg = {
                             type: "SleepStatus",
                             payload: status
                         };
                         node.send(msg);
                     });
-                    this.vacbot.on("CleanSpeed", (value) => {
+                    node.vacbot.on("CleanSpeed", (value) => {
                         let msg = {
                             type: "CleanSpeed",
                             payload: value
                         };
                         node.send(msg);
                     });
-                    this.vacbot.on("Error", (value) => {
+                    node.vacbot.on("Error", (value) => {
                         let msg = {
                             type: "Error",
                             payload: value
                         };
                         node.send(msg);
                     });
-                    this.vacbot.on("ErrorCode", (value) => {
+                    node.vacbot.on("ErrorCode", (value) => {
                         let msg = {
                             type: "ErrorCode",
                             payload: value
@@ -124,7 +124,7 @@ module.exports = function (RED) {
                         node.send(msg);
                     });
                 });
-                this.vacbot.connect_and_wait_until_ready();
+                node.vacbot.connect_and_wait_until_ready();
             });
         }).catch((e) => {
             node.status({
