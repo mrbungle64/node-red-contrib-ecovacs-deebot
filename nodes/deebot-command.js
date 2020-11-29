5524e3d7b7fad09dc3d5a665a703b57f0ff3444c
mrbungle64 <mrb1232@posteo.de>
2020-11-29 14:48:35 +0100

    Bugfix

--- nodes/deebot-command.js
+++ nodes/deebot-command.js
@@ -11,9 +11,7 @@ module.exports = function (RED) {
 
         function sendCommand(node) {
             node.send({
-                payload: {
-                    payload: node.config.command
-                }
+                payload: node.config.command
             })
         }
     }
