043e1a6aae1fdebeb5d624556fc398d727acd972
mrbungle64 <mrb1232@posteo.de>
2020-11-29 14:38:21 +0100

    Bugfix

--- nodes/deebot-command.js
+++ nodes/deebot-command.js
@@ -1,9 +1,10 @@
 module.exports = function (RED) {
-    function EcovacsDeebotCommandNode(n) {
-        RED.nodes.createNode(this, n);
-        this.command = n.command;
+    function EcovacsDeebotCommandNode(config) {
+        RED.nodes.createNode(this, config);
+        this.command = config.command;
 
         let node = this;
+        node.config = config;
         node.on('input', (msg) => {
             sendCommand(node);
         });
