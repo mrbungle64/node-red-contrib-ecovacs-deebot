6bf66c3e1e8b59afaaceb326f51db136084ba1df
mrbungle64 <mrb1232@posteo.de>
2020-11-29 14:30:31 +0100

    Start implement sending commands

--- nodes/deebot-command.js
+++ nodes/deebot-command.js
@@ -1,7 +1,21 @@
-module.exports = function(RED) {
+module.exports = function (RED) {
     function EcovacsDeebotCommandNode(n) {
-        RED.nodes.createNode(this,n);
+        RED.nodes.createNode(this, n);
         this.command = n.command;
+
+        let node = this;
+        node.on('input', (msg) => {
+            sendCommand(node);
+        });
+
+        function sendCommand(node) {
+            node.send({
+                payload: {
+                    payload: node.config.command
+                }
+            })
+        }
     }
-    RED.nodes.registerType("ecovacs-deebot-command",EcovacsDeebotCommandNode);
+
+    RED.nodes.registerType("ecovacs-deebot-command", EcovacsDeebotCommandNode);
 }
