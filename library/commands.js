eeb86a7951fd4fda51eb5d6cac501c7487e347cd
mrbungle64 <mrb1232@posteo.de>
2020-12-12 16:14:36 +0100

    Update commands.js

--- library/commands.js
+++ library/commands.js
@@ -2,20 +2,112 @@ module.exports = {
     "Clean": {
         payload: "Clean"
     },
+    "Edge": {
+        payload: "Edge"
+    },
+    "Spot": {
+        payload: "Spot"
+    },
+    "SpotArea": {
+        payload: "SpotArea",
+        arg: "start",
+        arg2: {
+            type: "string",
+            required: true
+        }
+    },
+    "CustomArea": {
+        payload: "CustomArea",
+        arg: {
+            type: "string",
+            required: true
+        },
+        arg2: {
+            name: "cleanings",
+            type: "number",
+            required: false
+        }
+    },
+    "Charge": {
+        payload: "Charge"
+    },
     "Stop": {
         payload: "Stop"
     },
     "Pause": {
         payload: "Pause"
     },
-    "Charge": {
-        payload: "Charge"
+    "PlaySound": {
+        payload: "PlaySound"
+    },
+    "FindMe": {
+        payload: "PlaySound",
+        arg: "30"
+    },
+    "Resume": {
+        payload: "Resume"
+    },
+    "GetCleanState": {
+        payload: "GetCleanState"
+    },
+    "GetChargeState": {
+        payload: "GetChargeState"
+    },
+    "GetBatteryState": {
+        payload: "GetBatteryState"
+    },
+    "GetCleanSpeed": {
+        payload: "GetCleanSpeed"
     },
     "GetWaterLevel": {
         payload: "GetWaterLevel"
     },
+    "SetWaterLevel": {
+        payload: "SetWaterLevel"
+    },
+    "GetWaterBoxInfo": {
+        payload: "GetWaterBoxInfo"
+    },
+    "GetNetInfo": {
+        payload: "GetNetInfo"
+    },
+    "GetChargerPos": {
+        payload: "GetChargerPos"
+    },
+    "GetCleanSum": {
+        payload: "GetCleanSum"
+    },
+    "Move": {
+        payload: "Move",
+        arg: {
+            type: "string",
+            required: true
+        }
+    },
+    "MoveBackward": {
+        payload: "MoveBackward"
+    },
+    "MoveForward": {
+        payload: "MoveForward"
+    },
+    "MoveLeft": {
+        payload: "MoveLeft"
+    },
+    "MoveRight": {
+        payload: "MoveRight"
+    },
+    "MoveTurnAround": {
+        payload: "MoveTurnAround"
+    },
+    "ResetLifeSpan": {
+        payload: "ResetLifeSpan"
+    },
     "GetLifeSpan": {
-        payload: "GetLifeSpan"
+        payload: "GetLifeSpan",
+        arg: {
+            type: "string",
+            required: true
+        }
     },
     "GetLifeSpan_filter": {
         label: "GetLifeSpan (filter)",
@@ -32,15 +124,21 @@ module.exports = {
         payload: "GetLifeSpan",
         arg: "side_brush"
     },
-    "SpotArea": {
-        payload: "SpotArea",
-        arg: "start"
+    "GetLogs": {
+        payload: "GetLogs",
+        arg: {
+            type: "string",
+            required: false
+        }
     },
-    "PlaySound": {
-        payload: "PlaySound"
+    "GetCleanLogs": {
+        payload: "GetCleanLogs",
+        arg: {
+            type: "string",
+            required: false
+        }
     },
-    "FindMe": {
-        payload: "PlaySound",
-        arg: "30"
-    }
+    "GetLogApiCleanLogs": {
+        payload: "GetLogApiCleanLogs"
+    },
 }
