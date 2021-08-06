5c01e72c6fce6f5de7bf83651675368ead5ad2c8
mrbungle64 <mrb1232@posteo.de>
2021-08-06 13:34:00 +0200

    Implement some tests for commands

--- test/test.js
+++ test/test.js
@@ -1,1 +1,21 @@
 const assert = require('assert');
+
+const resourcesCommands = require('../resources/commands');
+const germanTranslation = require('../nodes/locales/de/deebot-command.json')["ecovacs-deebot-command"];
+const englishTranslation = require('../nodes/locales/en/deebot-command.json')["ecovacs-deebot-command"];
+
+describe('Commands', () => {
+    for (const command in resourcesCommands.commands) {
+        describe('For command ' + command, () => {
+            it('should find a payload', function () {
+                assert.ok(resourcesCommands.commands[command].hasOwnProperty('payload'));
+            });
+            it('should find a German translation ', function () {
+                assert.ok(germanTranslation.hasOwnProperty(command));
+            });
+            it('should find a English translation ', function () {
+                assert.ok(englishTranslation.hasOwnProperty(command));
+            });
+        });
+    }
+});
